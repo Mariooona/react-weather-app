329cc0deb8fe6c86ca03d777bf8f992a43292a2f
Mariooona <101356811+Mariooona@users.noreply.github.com>
2022-10-09 18:30:37 +0300

    fix the footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://cosmic-faun-333eb7.netlify.app/"
+          className="portfolio-link"
+          rel="Portfolio page link"
+          target="_blank"
+        >
+          Alina-Marija Lapčiuk
+        </a>{" "}
+        and{" "}
+        <a
+          className="gitHub-link"
+          href="https://github.com/Mariooona/react-weather-app"
+          rel="GitHub link"
+          target="_blank"
+        >
+          is open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
