68c4dd5142f0cb1f6ff4afecb0524a062893eb8a
Mariooona <101356811+Mariooona@users.noreply.github.com>
2022-12-01 14:15:07 +0200

    update React package

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -17,21 +17,12 @@ export default function weatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix weather-temperature">
-            <WeatherTemperature celcius={props.data.temperature} />
-            <strong className="degreesDay">
-              {Math.round(props.data.temperatureDay)}{" "}
-            </strong>
-            <small className="degreesNight">
-              {" "}
-              {Math.round(props.data.temperatureNight)}{" "}
-            </small>
-            <span className="units">
-              <a href="/" className="active">
-                °C{" "}
-              </a>{" "}
-              |<a href="/">°F</a>
-            </span>{" "}
-            <WeatherIcon code={props.data.icon} />
+            <div className="float-left">
+              <WeatherTemperature celcius={props.data.temperature} />
+            </div>
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
           </div>
         </div>
         <div className="col-6">
